8754be78390d5c52314af9ae83f340651aa5e155
72lions <info@72lions.com>
2011-07-02 21:12:53 +0200

    Some code formatting

--- src/CanvasDisplayObject.js
+++ src/CanvasDisplayObject.js
@@ -5,134 +5,134 @@
  * @author Thodoris Tsiridis
  */
 
-var STL						= STL || {};
-STL.CanvasDisplayObject		= function(context) {
+var STL                     = STL || {};
+STL.CanvasDisplayObject     = function(context) {
 
-	var _children			= [],
-	_ctx					= context;
+    var _children           = [],
+    _ctx                    = context;
 
-	/**
-	 * The name of the display object
-	 */
-	this.name				= '';
-	this.x					= 0;
-	this.y					= 0;
-	this.rotation			= 0;
+    /**
+     * The name of the display object
+     */
+    this.name               = '';
+    this.x                  = 0;
+    this.y                  = 0;
+    this.rotation           = 0;
 
-	this._originX			= 0;
-	this._originY			= 0;
-	this._originRotation	= 0;
+    this._originX           = 0;
+    this._originY           = 0;
+    this._originRotation    = 0;
 
-	/**
-	 * The parent object
-	 */
-	this.parent				= null;
+    /**
+     * The parent object
+     */
+    this.parent             = null;
 
-	//Initialize
-	this.initialize();
+    //Initialize
+    this.initialize();
 
-	/**
-	 * ---------------------------------
-	 * PUBLIC FUNCTIONS
-	 * ---------------------------------
-	 */
+    /**
+     * ---------------------------------
+     * PUBLIC FUNCTIONS
+     * ---------------------------------
+     */
 
-	/**
-	 * Initializes the class
-	 */
-	this.initialize = function() {
+    /**
+     * Initializes the class
+     */
+    this.initialize = function() {
 
-	};
-	/**
-	 * Adds a child to the display object
-	 *
-	 * @param {CanvasDisplayObject} child The display object to add as a child
-	 */
-	this.addChild = function(child) {
+    };
+    /**
+     * Adds a child to the display object
+     *
+     * @param {CanvasDisplayObject} child The display object to add as a child
+     */
+    this.addChild = function(child) {
 
-		//Check if the child doesn't already exist
-		if (_children.indexOf(child) === - 1) {
+        //Check if the child doesn't already exist
+        if (_children.indexOf(child) === - 1) {
 
-			//Check if the child already has a parent
-			if( child.parent !== null ) {
+            //Check if the child already has a parent
+            if( child.parent !== null ) {
 
-				//If it already has a parent then remove it from it's parent
-				child.parent.removeChild( child );
+                //If it already has a parent then remove it from it's parent
+                child.parent.removeChild( child );
 
-			}
+            }
 
-			//Set the parent of the child
-			child.parent = this;
+            //Set the parent of the child
+            child.parent = this;
 
-			//Set the origin from the parent
-			child._originX = this.x;
-			child._originY = this.y;
-			child._originRotation = this.rotation;
+            //Set the origin from the parent
+            child._originX = this.x;
+            child._originY = this.y;
+            child._originRotation = this.rotation;
 
-			//Push the child in the array
-			_children.push( child );
+            //Push the child in the array
+            _children.push( child );
 
-		}
+        }
 
-	};
-	/**
-	 * Removes a child
-	 *
-	 * @param {CanvasDisplayObject} child  The display object to remove
-	 */
-	this.removeChild = function(child) {
+    };
+    /**
+     * Removes a child
+     *
+     * @param {CanvasDisplayObject} child  The display object to remove
+     */
+    this.removeChild = function(child) {
 
-		var childIndex = _children.indexOf( child );
+        var childIndex = _children.indexOf( child );
 
-		//Check the child index
-		if (  childIndex !== - 1 ) {
+        //Check the child index
+        if (  childIndex !== - 1 ) {
 
-			child.parent = null;
+            child.parent = null;
 
-			//Remove the child from the children array
-			_children.splice( childIndex, 1 );
+            //Remove the child from the children array
+            _children.splice( childIndex, 1 );
 
-		}
-	};
-	/**
-	 * Draws on the stage
-	 */
-	this.draw = function() {
+        }
+    };
+    /**
+     * Draws on the stage
+     */
+    this.draw = function() {
 
-	};
-	/**
-	 * Returns an array with all the children
-	 *
-	 * @returns {[CanvasDisplayObject]} The array with all the children
-	 */
-	this.getChildren = function() {
-		return _children;
-	};
-	/**
-	 * ---------------------------------
-	 * PRIVATE FUNCTIONS
-	 * ---------------------------------
-	 */
-	this._update = function() {
+    };
+    /**
+     * Returns an array with all the children
+     *
+     * @returns {[CanvasDisplayObject]} The array with all the children
+     */
+    this.getChildren = function() {
+        return _children;
+    };
+    /**
+     * ---------------------------------
+     * PRIVATE FUNCTIONS
+     * ---------------------------------
+     */
+    this._update = function() {
 
-		//Save the current translation, rotation
-		_ctx.save();
+        //Save the current translation, rotation
+        _ctx.save();
 
-		//Rotate and translate
-		_ctx.translate(this._originX + this.x, this._originY + this.y);
-		_ctx.rotate(this._originRotation + this.rotation);
+        //Rotate and translate
+        _ctx.translate(this._originX + this.x, this._originY + this.y);
+        _ctx.rotate(this._originRotation + this.rotation);
 
-		this.draw();
+        this.draw();
 
-		//Restore the translation, rotation
-		_ctx.restore();
-		
-		//Invoke the draw function for each child
-		for(var d = 0; d < _children.length; d++) {
-			_children[d]._update();
-		}
+        //Restore the translation, rotation
+        _ctx.restore();
 
-		d = null;
-				
-	};
+        //Invoke the draw function for each child
+        for(var d = 0; d < _children.length; d++) {
+            _children[d]._update();
+        }
+
+        d = null;
+
+    };
 };
