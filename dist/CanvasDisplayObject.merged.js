668e87feff3a0632960813388579ea9d3e8666b3
72lions <info@72lions.com>
2011-10-16 13:18:40 +0200

    Modified folders structure

---
+++ dist/CanvasDisplayObject.merged.js
@@ -0,0 +1,134 @@
+/**
+ *
+ * Simulates a display object just like in AS3
+ *
+ * @author Thodoris Tsiridis
+ */
+
+var STL                     = STL || {};
+STL.CanvasDisplayObject     = function() {
+
+    var _children           = [];
+
+    /**
+     * Public members
+     */
+    this.name               = '';
+
+    this.x                  = 0;
+    this.y                  = 0;
+    this.rotation           = 0;
+    this.scaleX             = 1;
+    this.scaleY             = 1;
+    this.visible            = true;
+    this.alpha              = 1;
+    this.extra              = {};
+
+    /**
+     * The parent object
+     */
+    this.parent             = null;
+
+    /**
+     * ---------------------------------
+     * PUBLIC FUNCTIONS
+     * ---------------------------------
+     *
+     * Adds a child to the display object
+     *
+     * @param {CanvasDisplayObject} child The display object to add as a child
+     */
+    this.addChild = function(child) {
+
+        //Check if the child doesn't already exist
+        if (_children.indexOf(child) === - 1) {
+
+            //Check if the child already has a parent
+            if( child.parent !== null ) {
+
+                //If it already has a parent then remove it from it's parent
+                child.parent.removeChild( child );
+
+            }
+
+            //Set the parent of the child
+            child.parent = this;
+
+            //Push the child in the array
+            _children.push( child );
+
+        }
+
+    };
+    /**
+     * Removes a child
+     *
+     * @param {CanvasDisplayObject} child  The display object to remove
+     */
+    this.removeChild = function(child) {
+
+        var childIndex = _children.indexOf( child );
+
+        //Check the child index
+        if (  childIndex !== - 1 ) {
+
+            child.parent = null;
+
+            //Remove the child from the children array
+            _children.splice( childIndex, 1 );
+
+        }
+    };
+    /**
+     * Returns an array with all the children
+     *
+     * @returns {[CanvasDisplayObject]} The array with all the children
+     */
+    this.getChildren = function() {
+        return _children;
+    };
+    /**
+     * Updates the object
+     *
+     * @param {CanvasContext} ctx The context on which everything will be drawn
+     */
+    this.update = function(ctx) {
+
+        if(this.visible !== false) {
+
+            //Save the current translation, rotation
+            ctx.save();
+
+            //Translate Scale and Rotate
+            ctx.translate(this.x, this.y);
+            ctx.scale(this.scaleX,this.scaleY);
+            ctx.rotate(this.rotation);
+            ctx.globalAlpha = ctx.globalAlpha * this.alpha;
+
+
+            this.draw();
+
+            //Invoke the update function for each child
+            var d = 0;
+
+            while(d < _children.length) {
+
+                _children[d].update(ctx);
+
+                d++;
+            }
+
+            //Restore the translation, rotation
+            ctx.restore();
+
+            d = null;
+
+        }
+    };
+};
+/**
+ * Generic function for overwritting and adding the your code
+ */
+STL.CanvasDisplayObject.prototype.draw = function() {
+
+};
---
+++ examples/scaling-objects/scaling-boxes.js
@@ -0,0 +1,141 @@
+/**
+ * @author Thodoris Tsiridis
+ */
+
+var canvas;
+var context;
+var stage;
+
+var rect;
+
+var rect2;
+var rect3;
+var rect4;
+var rect5;
+
+var CANVAS_WIDTH        = 900;
+var CANVAS_HEIGHT       = 600;
+
+var t                   = 0;
+
+/**
+ * Initializing the stage
+ */
+var initialize = function() {
+
+    //Create the canvas & the context
+    canvas = document.getElementById('world');
+    context = canvas.getContext('2d');
+
+    //Create the first object that will call it stage
+    stage = new STL.CanvasDisplayObject();
+    stage.name = 'Stage';
+
+    //Moving the stage
+    stage.x = 0;
+    stage.y = 0;
+
+    //Creating a new display object
+    rect = new STL.CanvasDisplayObject();
+    rect.name = 'Rectangular';
+    rect.y = 300;
+    rect.x = 450;
+
+    //Scaling the new display object
+    rect.scaleX = 1;
+    rect.scaleY = 1;
+
+    //Overwrite the draw function of the display object
+    rect.draw = function() {
+        context.fillStyle = "rgb(200,0,0)";
+        context.fillRect(-50,-50, 100, 100);
+    };
+    //Creating a new display object
+    rect2 = new STL.CanvasDisplayObject();
+    rect2.name = 'Purple Rectangular';
+
+    //Positioning and scaling the new object
+    rect2.x = -50;
+    rect2.y = -50;
+
+    //Overwrite the draw function of the display object
+    rect2.draw = function() {
+        context.fillStyle = "rgb(150,0,150)";
+        context.fillRect(-13, -13, 26, 26);
+    };
+    //Creating a new display object
+    rect3 = new STL.CanvasDisplayObject();
+    rect3.name = 'Blue Rectangular';
+
+    //Positioning and scaling the new object
+    rect3.x = 50;
+    rect3.y = -50;
+
+    //Overwrite the draw function of the display object
+    rect3.draw = function() {
+        context.fillStyle = "rgb(0,0,255)";
+        context.fillRect(-13, -13, 26, 26);
+    };
+    //Creating a new display object
+    rect4 = new STL.CanvasDisplayObject();
+    rect4.name = 'Dark Green Rectangular';
+
+    //Positioning and scaling the new object
+    rect4.x = -50;
+    rect4.y = 50;
+
+    //Overwrite the draw function of the display object
+    rect4.draw = function() {
+        context.fillStyle = "rgb(50,100,20)";
+        context.fillRect(-13, -13, 26, 26);
+    };
+    //Creating a new display object
+    rect5 = new STL.CanvasDisplayObject();
+    rect5.name = 'Green Rectangular';
+
+    //Positioning and scaling the new object
+    rect5.x = 50;
+    rect5.y = 50;
+
+    //Overwrite the draw function of the display object
+    rect5.draw = function() {
+        context.fillStyle = "rgb(0,255,0)";
+        context.fillRect(-13, -13, 26, 26);
+    };
+    //Add the first object into the stage
+    stage.addChild(rect);
+
+    //Add all the other object into the first object
+    rect.addChild(rect2);
+    rect.addChild(rect3);
+    rect.addChild(rect4);
+    rect.addChild(rect5);
+
+    //Set the canvas width
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+
+    //Start drawing
+    setInterval(draw, 1000/60);
+
+};
+var draw = function() {
+    t++;
+
+    //Clear the canvas
+    canvas.width = CANVAS_WIDTH;
+
+    //Move the first object
+    //rect.x = 400 + (-Math.cos(t * Math.PI / 180) * 0.5) * 500;
+    rect.scaleX = rect.scaleY = -Math.cos(t * Math.PI / 180) * 0.5 + 1;
+
+    rect2.rotation += 0.1;
+    rect3.rotation += 0.1;
+    rect4.rotation += 0.1;
+    rect5.rotation += 0.1;
+
+    //Update the stage
+    stage.update(context);
+
+};
+initialize();
